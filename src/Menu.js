e1055a643ec939fa03a243663a190fc3691fe4e8
YosefLun <YosefHoukit@yesmalot.co.il>
2022-11-20 18:47:55 +0200

    first commit

---
+++ src/Menu.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+function Menu(props) {
+  return (
+    <div className="menu">
+      <h1 className='page-title'>Quizzical</h1>
+      <span className='page-description'>Description</span>
+      <button className='start-button' onClick={() => props.start()}>Start Quizzical</button>
+    </div>
+  )
+}
+
+export default Menu
